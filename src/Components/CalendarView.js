a78d1fe46cd90b1c250add59ba9a7636d26beb6a
Zaid Humayun <zaidhumayun@Zaids-MacBook-Pro.local>
2018-12-02 19:32:49 +0530

    positioning of divs is correct for appointment timing

--- src/Components/CalendarView.js
+++ src/Components/CalendarView.js
@@ -3,10 +3,29 @@ import './CalendarView.css'
 
 export default class CalendarView extends Component {
   render() {
-    const { appointments } = this.props
+    const { appointmentCollection } = this.props
     return (
       <div className="calendar">
-      Hello
+        {
+          appointmentCollection.map((appointments) => {
+            return appointments.map((appointment, index) => {
+              console.log(appointment)
+              return (
+                <div 
+                className="appointment"
+                key={index}
+                style={{
+                  width: appointment.width,
+                  height: appointment.height,
+                  marginLeft: appointment.marginLeft,
+                  marginTop: appointment.marginTop,
+                  backgroundColor: 'blue'
+                }}>
+                </div>
+              )
+            })
+          })
+        }
       </div>
     )
   }
--- src/Containers/Calendar.js
+++ src/Containers/Calendar.js
@@ -6,31 +6,78 @@ import './Calendar.css'
 export default class Calendar extends Component {
   render() {
     const { appointments } = this.props
-    const result = this.computeTimeClashes()
-    console.log(result)
+    const result = this.computeTimeClashes(appointments)
+    const result2 = this.separateBlocks(result)
+    const result3 = this.calculateDimensions(result2)
     return (
       <div className="container">
         <TimeLineView />
-        <CalendarView appointments={appointments} />
+        <CalendarView appointmentCollection={result3} />
       </div>
     )
   }
 
-  computeTimeClashes() {
-    const { appointments } = this.props   
+  /**
+   * This function will check the start of every object with the end of the prior object
+   * Will update a clashes property within each object. 
+   * @param {Array} appointments
+   * @return {Array}
+   */
+  computeTimeClashes(appointments) {
     const appointmentsCopy = appointments.map((appointment) => {
       return Object.assign({}, {...appointment})
     })
     for(let i = 1; i < appointmentsCopy.length; i++) {
-      if (this.convertToMins(appointmentsCopy[i].start) < this.convertToMins(appointmentsCopy[i-1].end) ) {
+      if (this.convertToMinsElapsed(appointmentsCopy[i].start) < this.convertToMinsElapsed(appointmentsCopy[i-1].end) ) {
         appointmentsCopy[i].clashes = 1
         appointmentsCopy[i-1].clashes = 1
       }
     }
-    return appointmentsCopy
+    return appointmentsCopy.map((appointment) => {
+      if(!appointment.clashes) {
+        appointment.clashes = 0
+      }
+      return appointment
+    })
   }
 
-  convertToMins(time) {
+  /**
+   * This function will separate the appointments array of objects into 
+   * multiple arrays of continuous appointment blocks. Any appointments that all have a clash in a sequence are separated. 
+   * Standalone appointments with 0 clashes will be in a separate array
+   * @param {Array} appointments 
+   */
+  separateBlocks(appointments) {
+    let collectionArr = []
+    let tempArr = []
+    appointments.map((appointment) => {
+      if(appointment.clashes === 0) {
+        collectionArr.push(tempArr)
+        tempArr = []
+        tempArr.push(appointment)
+        collectionArr.push(tempArr)
+        tempArr = []
+      } else if(appointment.clashes === 1) {
+        tempArr.push(appointment)
+      }
+    })
+    collectionArr.push(tempArr)
+    return collectionArr.filter(arr => arr.length > 0)
+  }
+
+  calculateDimensions(appointmentsCollection) {
+    return appointmentsCollection.map(appointments => {
+      return appointments.map((appointment, index) => {
+        appointment.width = 600 / appointments.length
+        appointment.height = this.convertToMinsElapsed(appointment.end) - this.convertToMinsElapsed(appointment.start)
+        appointment.marginLeft = appointment.width * index
+        appointment.marginTop = this.convertToMinsElapsed(appointment.start)
+        return appointment
+      })
+    })
+  }
+
+  convertToMinsElapsed(time) {
     const referenceTime = 9 * 60
     time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)/) || [time]
     if (time.length > 1) {
@@ -39,45 +86,4 @@ export default class Calendar extends Component {
       return minsElapsed
     }
   }
-
-  /**
-   * 
-   * @param {Array} appointments 
-   */
-  getMinutesArray(appointments) {
-    return appointments.reduce((acc, curr) => {
-      const mins = this.minutesElapsed(curr)
-      acc.push(...mins)
-      return acc
-    }, [])
-  }
-
-  /**
-   * Find minutes elapsed since 09:00
-   * @param {Object} appointmentObj
-   * @return {Array}
-   */
-  minutesElapsed(appointmentObj) {
-    const referenceTime = 9 * 60
-    return Object.keys(appointmentObj).reduce((acc, key) => {
-      let time = appointmentObj[key].toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)/) || [appointmentObj[key]]
-      if (time.length > 1) {
-        time = time.slice(1)
-        let minsElapsed = +time[0] * 60 + (+time[2]) - referenceTime
-        acc.push(minsElapsed)
-      }
-      return acc
-    }, [])
-  }
-
-  // createTimeArray() {
-  //   const start = '09:00'
-  //   const duration = '12:00'
-  //   let current = +start.substr(0, 2) - 1
-  //   let iterations = +duration.substr(0, 2)
-  //   return new Array(iterations + 1).fill(0).map(() => {
-  //     current += 1
-  //     return current < 10 ? `0${current}:00` : `${current}:00`
-  //   })
-  // }
 }
